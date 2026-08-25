c3dfcfba52880f5d79102d5349a0d547e0e58c64
Farhaan <farhaan654@gmail.com>
2026-08-25 12:03:15 -0600

    refactor: changed structure to src/ and tests/

---
+++ src/ssg/htmlnode.py
@@ -0,0 +1,32 @@
+class HTMLNode:
+    def __init__(
+        self,
+        tag: str | None = None,
+        value: str | None = None,
+        children: list[HTMLNode] | None = None,
+        props: dict | None = None,
+    ):
+        self.tag: str | None = tag
+        self.value: str | None = value
+        self.children: list[HTMLNode] | None = children
+        self.props: dict | None = props
+
+    def to_html(self):
+        raise NotImplementedError("HTMLNode.to_html() not implmented yet")
+
+    def props_to_html(self) -> str:
+        if self.props == None:
+            return ""
+
+        props_str_repr = ""
+        for key, val in self.props:
+            props_str_repr += f' {key}:"{val}"'
+        return props_str_repr
+
+    def __repr__(self) -> str:
+        return f"""
+        Tag: {self.tag}
+        Value: {self.value}
+        Children: {self.children}
+        Props: {self.props}
+        """
--- tests/test_textnode.py
+++ tests/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from ssg.textnode import TextNode, TextType
 
 
 class TestTextNode(unittest.TestCase):
