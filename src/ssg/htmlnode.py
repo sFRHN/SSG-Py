d0bcfc7cb80d1fde079e846d28f1c9d72a31f496
Farhaan <farhaan654@gmail.com>
2026-08-25 12:26:42 -0600

    test: added unit tests for HTMLNode

--- src/ssg/htmlnode.py
+++ src/ssg/htmlnode.py
@@ -23,6 +23,14 @@ class HTMLNode:
             props_str_repr += f' {key}:"{val}"'
         return props_str_repr
 
+    def __eq__(self, other) -> bool:
+        return (
+            self.tag == other.tag
+            and self.value == other.value
+            and self.children == other.children
+            and self.props == other.props
+        )
+
     def __repr__(self) -> str:
         return f"""
         Tag: {self.tag}
---
+++ tests/test_htmlnode.py
@@ -0,0 +1,75 @@
+import unittest
+
+from ssg.htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_tag(self):
+        node = HTMLNode(tag="p")
+        self.assertEqual(node.tag, "p")
+
+    def test_values(self):
+        node = HTMLNode(value="test")
+        self.assertEqual(node.value, "test")
+
+    def test_children(self):
+        child = HTMLNode()
+        node = HTMLNode(children=[child])
+        self.assertEqual(node.children, [child])
+
+    def test_props(self):
+        test_props: dict = {"href": "test.com"}
+        node = HTMLNode(props=test_props)
+        self.assertEqual(node.props, test_props)
+
+    def test_EmptyNodes(self):
+        node1 = HTMLNode()
+        node2 = HTMLNode()
+        self.assertEqual(node1, node2)
+
+    def test_EqTags(self):
+        node1 = HTMLNode(tag="p")
+        node2 = HTMLNode(tag="p")
+        self.assertEqual(node1, node2)
+
+    def test_EqValues(self):
+        node1 = HTMLNode(value="test")
+        node2 = HTMLNode(value="test")
+        self.assertEqual(node1, node2)
+
+    def test_EqChildren(self):
+        child = HTMLNode()
+        node1 = HTMLNode(children=[child])
+        node2 = HTMLNode(children=[child])
+        self.assertEqual(node1, node2)
+
+    def test_EqProps(self):
+        test_props: dict = {"href": "test"}
+        node1 = HTMLNode(props=test_props)
+        node2 = HTMLNode(props=test_props)
+        self.assertEqual(node1, node2)
+
+    def test_DiffTags(self):
+        node1 = HTMLNode("p")
+        node2 = HTMLNode("a")
+        self.assertNotEqual(node1, node2)
+
+    def test_DiffValues(self):
+        node1 = HTMLNode(value="val1")
+        node2 = HTMLNode(value="val2")
+        self.assertNotEqual(node1, node2)
+
+    def test_DiffChildren(self):
+        child1 = HTMLNode(tag="p")
+        node1 = HTMLNode(children=[child1])
+        child2 = HTMLNode(tag="a")
+        node2 = HTMLNode(children=[child2])
+        self.assertNotEqual(node1, node2)
+
+    def test_DiffProps(self):
+        test_props1: dict = {"href": "test1"}
+        node1 = HTMLNode(props=test_props1)
+
+        test_props2: dict = {"href": "test2"}
+        node2 = HTMLNode(props=test_props2)
+        self.assertNotEqual(node1, node2)
