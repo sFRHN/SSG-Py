7c28d8cac19f07df1608aaef28ee706eb0181f22
Farhaan <farhaan654@gmail.com>
2026-08-26 13:05:33 -0600

    feat: implemented 'markdown_to_blocks' to convert MD text into individual blocks. Also added unit tests

--- src/ssg/markdown_extract.py
+++ src/ssg/markdown_extract.py
@@ -1,13 +1,26 @@
 import re
 
 """
-Simple functions to extract info from Markdown links and images
+Simple functions to extract info from Markdown
 """
+
 
 def extract_markdown_images(text: str) -> list[tuple[str, str]]:
     matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
 
+
 def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
+
+def markdown_to_blocks(markdown: str) -> list[str]:
+    blocks = []
+    for block in markdown.split("\n\n"):
+        stripped_block = block.strip()
+        if stripped_block == "":
+            continue
+        blocks.append(stripped_block)
+
+    return blocks
