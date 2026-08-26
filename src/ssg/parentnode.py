f8fc66c9010c03a48bbdf4648f14715610c3b0c3
Farhaan <farhaan654@gmail.com>
2026-08-25 18:24:14 -0600

    feat: implemented ParentNode - stores multiple nodes

---
+++ src/ssg/parentnode.py
@@ -0,0 +1,20 @@
+from ssg.htmlnode import HTMLNode
+
+
+class ParentNode(HTMLNode):
+
+    def __init__(self, tag: str, children: list[HTMLNode], props: dict[str, str] | None = None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self) -> str:
+        if not self.tag:
+            raise ValueError("ParentNode has no tag")
+
+        if not self.children:
+            raise ValueError("ParentNode has no children")
+
+        chilldren_str = ""
+        for node in self.children:
+            chilldren_str += node.to_html()
+        final_str = f"<{self.tag}{self.props_to_html()}>{chilldren_str}</{self.tag}>"
+        return final_str
