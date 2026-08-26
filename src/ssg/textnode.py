7e443ecadd69d55a3f6b7fe0a3776ce7c8a64b09
Farhaan <farhaan654@gmail.com>
2026-08-25 21:27:06 -0600

    feat: implemented textnode to HTMLNode conversion function and added unit tests

--- src/ssg/textnode.py
+++ src/ssg/textnode.py
@@ -1,18 +1,22 @@
 from enum import Enum
 
+from ssg.leafnode import LeafNode
+
 
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 
 class TextNode:
-    def __init__(self, text: str, text_type: TextType, url: str) -> None:
+    def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
         self.text: str = text
         self.text_type: TextType = text_type
-        self.url: str = url
+        self.url: str | None = url
 
     def __eq__(self, other) -> bool:
         return (
@@ -23,3 +27,26 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(textnode: TextNode) -> LeafNode:
+
+    match textnode.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None, value=textnode.text)
+        case TextType.BOLD:
+            return LeafNode(tag="b", value=textnode.text)
+        case TextType.ITALIC:
+            return LeafNode(tag="i", value=textnode.text)
+        case TextType.CODE:
+            return LeafNode(tag="code", value=textnode.text)
+        case TextType.LINK:
+            if textnode.url == None:
+                raise ValueError("LINK text node requires a url")
+            return LeafNode(tag="a", value=textnode.text, props = {"href": textnode.url})
+        case TextType.IMAGE:
+            if textnode.url == None:
+                raise ValueError("IMAGE text node requires a url")
+            return LeafNode(tag="img", value="", props = { "src": textnode.url, "alt": textnode.text})
+        case _:
+            raise TypeError("TextNode - Invalid Type")
+        
--- tests/test_textnode.py
+++ tests/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ssg.textnode import TextNode, TextType
+from ssg.textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -29,6 +29,50 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a test", TextType.BOLD, "")
         self.assertNotEqual(node1, node2)
 
+    def test_textToHTML_TEXT(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_textToHTML_BOLD(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+    def test_textToHTML_ITALIC(self):
+        node = TextNode("This is italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is italic")
+
+    def test_textToHTML_CODE(self):
+        node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is code")
+
+    def test_textToHTML_LINK(self):
+        node = TextNode("Click me", TextType.LINK, "https://google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me")
+        self.assertEqual(html_node.props, {"href": "https://google.com"})
+
+    def test_textToHTML_IMAGE(self):
+        node = TextNode("Alt text", TextType.IMAGE, "https://img.com/pic.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props, {"src": "https://img.com/pic.png", "alt": "Alt text"}
+        )
+
+    def test_textToHTML_invalid(self):
+        node = TextNode("Text", "not-a-valid-type")
+        with self.assertRaises(TypeError):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
