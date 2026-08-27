a65aafc3d988f74cd2b80ba0dfddcdc9e52430a3
Farhaan <farhaan654@gmail.com>
2026-08-27 03:40:56 -0600

    feat: calling generate_page

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from ssg.copy_files import copy_files
+from ssg.generate_page import generate_page
 
 
 def main():
     copy_files("static", "public")
-
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 if __name__ == "__main__":
     main()
