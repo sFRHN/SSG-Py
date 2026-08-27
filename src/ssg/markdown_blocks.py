961e47cedc72f7e208544bbf392caf184aa64661
Farhaan <farhaan654@gmail.com>
2026-08-26 19:37:38 -0600

    feat: implemented 'block_to_blocktype' to categorize blocks

--- src/ssg/markdown_blocks.py
+++ src/ssg/markdown_blocks.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = []
     for block in markdown.split("\n\n"):
@@ -7,3 +19,25 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         blocks.append(stripped_block)
 
     return blocks
+
+def block_to_blocktype(block: str) -> BlockType:
+    
+    # Split into lines for checking QUOTE, UNORDERED_LIST and ORDERED_LIST
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    
+    if block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+
+    if all(lines[i].startswith(f"{i+1}. ") for i, _ in enumerate(lines)):
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
